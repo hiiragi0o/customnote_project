94c215383ba5d3cc1733beeb54b9171df6edcf06
Margherita88 <lilac.spring2403@gmail.com>
2025-07-20 11:58:02 +0900

    データベース設定を修正し、SQLiteの設定を辞書形式に変更

--- config/settings.py
+++ config/settings.py
@@ -89,8 +89,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 if DEBUG:
-    DATABASE_URL = 'sqlite:///' + str(BASE_DIR / 'db.sqlite3')
-    DATABASES = {'default': dj_database_url.config(default=DATABASE_URL)}
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 else:
     DATABASE_URL = env('DATABASE_URL')
     DATABASES = {'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=600, ssl_require=True)}
